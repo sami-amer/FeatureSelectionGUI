c70c50b4b58f6f6b0bc7f4210683aab40337e4bb
Sami Amer <59567234+samiatMIT@users.noreply.github.com>
2020-04-07 19:07:04 -0400

    created load_features function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # import sklearn
-
+import csv
 def load_labels(f):
     labels = []
     length = 0
-    with open(f) as f:
+    with open(f,'r') as f:
         for line in f:
             length += 1
             line = line.strip('\n')
@@ -11,9 +11,25 @@ def load_labels(f):
     return labels, length
 
 def load_features(f):
-    features=[]
-
+    features = []
+    length = 0
+    with open (f,'r') as f:
+        for line in f:
+            line = line.split(",")
+            line[-1] = line[-1].strip('\n')
+            featuresLength = len(line)
+            for feature in range(featuresLength):
+                features.append([line[feature]])
+            break
+        for line in f:
+            length += 1
+            line = line.split(",")
+            line[-1] = line[-1].strip('\n')
+            for feature in range(featuresLength):
+                features[feature].append(line[feature])
+    return features, length
 
 if __name__ == "__main__":
     # print(load_labels('resources/wellness_nonverbal_condition_lable.csv'))
+    # print(load_features('resources/wellness_nonverbal_features.csv'))
     pass
