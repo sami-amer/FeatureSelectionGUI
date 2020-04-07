ddb21f3ae4423c16158e738c6c5ceb891146d945
Sami Amer <59567234+samiatMIT@users.noreply.github.com>
2020-04-07 17:55:32 -0400

    added load label function

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import sklearn
+# import sklearn
 
+def load_labels(f):
+    labels = []
+    length = 0
+    with open(f) as f:
+        for line in f:
+            length += 1
+            line = line.strip('\n')
+            labels.append(float(line))
+    return labels, length
+
+def load_features(f):
+    features=[]
+    
+
+if __name__ == "__main__":
+    # print(load_labels('resources/wellness_nonverbal_condition_lable.csv'))
