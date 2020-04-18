6e3ebd57bec3ec2e0cb37f2ce5e5112fbabe6c74
Sami Amer <59567234+samiatMIT@users.noreply.github.com>
2020-04-18 07:43:16 -0400

    fixed tTest

--- featureSelections/tester.py
+++ featureSelections/tester.py
@@ -6,13 +6,11 @@ import numpy as np
 features = pd.read_csv(r'resources\wellness_nonverbal_features.csv')
 labels = pd.read_csv(r'resources\wellness_nonverbal_condition_lable.csv')
 
-#ccm needs: X,Y, num of features, type_y, epsilon
-X = features.to_numpy() # makes the data frame into a numpy array
+
+X = features.to_numpy()
 Y = labels.to_numpy()
-numFeatures = X.shape[1] # gets the number of columns in X
-type_Y = 'binary'
-epsilon = 0.1
+
 
 Y = Y.reshape((Y.shape[0],)) # makes it a (n,) array
 
-myTtest.Ttest(X,Y)
+# myTtest.Ttest(X,Y)
